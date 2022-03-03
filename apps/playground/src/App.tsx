550adb01ff988b799e6f41c8a70d99c83f23f881
Adam Aho <adam.aho@hey.com>
2022-03-02 20:38:51 -0500

    adding some more stuff

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -5,15 +5,18 @@ import {Button} from "@bonsai/solid-button";
 import * as styles from "./App.css";
 
 const App: Component = () => {
-
-  let foo: HTMLButtonElement | undefined;
-
-  onMount(() => {
-    console.log(foo);
-  });
-
   return (
-    <Button.Root className={styles.button} ref={foo} />
+    <Button.Root className={styles.button}>
+      <Button.Icon>
+        A
+      </Button.Icon>
+      <Button.Text>
+        hello
+      </Button.Text>
+      <Button.Icon>
+        B
+      </Button.Icon>
+    </Button.Root>
   );
 };
 
--- packages/button/src/Button.tsx
+++ packages/button/src/Button.tsx
@@ -1,19 +1,58 @@
 import type { Component } from "solid-js";
 
-/* -------------------------------------------------------
+/* -----------------------------------------------------------
 * ButtonRoot
 -----------------------------------------------------------**/
 interface ButtonRootProps {
-  className: string;
+  className?: string;
   ref?: HTMLButtonElement;
 }
 
-const ButtonRoot: Component<ButtonRootProps> = ({ className, ref }) => {
+const ButtonRoot: Component<ButtonRootProps> = ({ children, className, ref }) => {
   return (
-    <button className={className} ref={ref}>hello</button>
+    <button className={className} ref={ref}>
+      {children}
+    </button>
+  );
+};
+
+/* -----------------------------------------------------------
+* ButtonText
+-----------------------------------------------------------**/
+interface ButtonTextProps {
+  children: string;
+  className?: string;
+  ref?: HTMLSpanElement;
+}
+
+const ButtonText: Component<ButtonTextProps> = ({ children, className, ref }) => {
+  return (
+    <span className={className} ref={ref}>
+      {children}
+    </span>
+  );
+};
+
+/* -----------------------------------------------------------
+* ButtonIcon
+-----------------------------------------------------------**/
+interface ButtonIconProps {
+  className?: string;
+  ref?: HTMLSpanElement;
+}
+
+const ButtonIcon: Component<ButtonIconProps> = ({ children, className, ref }) => {
+  return (
+    <span className={className} ref={ref}>
+      {children}
+    </span>
   );
 };
 
 export const Button = {
-  Root: ButtonRoot
+  Root: ButtonRoot,
+  Text: ButtonText,
+  Icon: ButtonIcon
 };
+
+export type { ButtonRootProps, ButtonTextProps, ButtonIconProps };
