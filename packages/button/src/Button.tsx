4a525d4a0f3d3d6d9ebc270e92c9a5c397416385
Adam Aho <adam.aho@hey.com>
2022-03-02 19:35:30 -0500

    first commit

---
+++ packages/button/src/Button.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const ButtonRoot: Component = () => {
+  return (
+    <button>hello</button>
+  );
+};
+
+export const Button = {
+  Root: ButtonRoot
+};
